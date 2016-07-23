1bc9ca3cf25a5785ec6e1b5128707d33429d614d
loics2 <loicstankovic@hotmail.com>
2016-07-23 17:09:29 +0200

    Refactor to OO code
    The logic has been move to a class, simplifying the cli code. This will
    make the unit test easier.

---
+++ sorta/drop_folder.py
@@ -0,0 +1,141 @@
+"""This module contains everything needed to represent a Sorta drop folder"""
+
+import configparser
+import logging
+import os
+from shutil import move, Error
+
+
+CONFIG_FILE_NAME = ".sortaconfig"
+
+class DropFolder(object):
+
+    """This class is the representation of a Sorta drop folder.
+    
+    A Sorta drop folder is a folder managed by Sorta. It must contain a .sortaconfig file. 
+    The DropFolder object cannot be instanciated with an invalid drop folder (it will raise a FileNotFoundError).
+    
+    """
+
+    def __init__(self, path):
+        """Initialize a DropFolder object
+        
+        Args :
+            path (str) : path of the drop folder
+            
+        Raises :
+            FileNotFoundError : if the folder is not correctly initialized (the given path or the
+                                .sortaconfig  doesn't exist)
+                                
+        """
+        self.path = path
+        self.config_path = os.path.join(path, CONFIG_FILE_NAME)
+
+        self.config = configparser.ConfigParser()
+        if (not os.path.exists(self.path)) or (not os.path.exists(self.config_path)):
+            raise FileNotFoundError("this folder is not correctly initialized")
+        else:
+            self.config.read_file(open(self.config_path))
+
+        logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s [%(levelname)-5.5s]  %(message)s',
+                        handlers=[logging.StreamHandler()])
+
+    def _get_destination(self, element):
+        """Find the right destination corresponding to the element (file or folder) name.
+        
+        Args :
+            element (str) : name of the element to get the destination
+            
+        Returns :
+            dest (str) : the path corresponding to the prefix or extension of the element 
+            
+        Raises :
+            LookupError : if no destination has been found for the element in the config
+            
+        """
+        split_prefix = element.split(self.config.get('core', 'delimiter'))
+        split_ext = os.path.splitext(element)
+
+        try:
+            if len(split_prefix) == 2:
+                dest = os.path.join(self.config.get('prefix', split_prefix[0]), split_prefix[1])
+            elif len(split_ext) == 2:
+                dest = os.path.join(self.config.get('extension', split_ext[1][1:]), element)
+            else:
+                raise LookupError("no destination found for the element {}".format(element))
+        except configparser.NoOptionError:
+            raise LookupError("no destination found for the element {}".format(element))
+
+        return dest
+    
+    @staticmethod
+    def init_folder(path):
+        """Initialize a drop folder.
+        
+        This function creates the folder at the given path if needed, and write the default
+        .sortaconfig file. If a .sortaconfig file already exists, it will be resetted
+
+        """
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        config = configparser.ConfigParser()
+        config.add_section('core')
+        config.set('core', 'delimiter', '--')
+        config.set('core', 'polling', '60.0')
+
+        config.add_section('prefix')
+
+        config.add_section('extension')
+
+        with open(os.path.join(path, CONFIG_FILE_NAME), mode='w') as configfile:
+            config.write(configfile)
+    
+    def sort(self):
+        """Sort the files inside the drop folder."""
+        for elt in os.listdir(self.path):
+            if elt == CONFIG_FILE_NAME:
+                continue
+
+            try:
+                dest = self._get_destination(elt)
+                move(os.path.join(self.path, elt), dest)
+                logging.info("%s moved to %s", elt, dest)
+            except LookupError as e:
+                logging.warning(str(e))
+            except Error:
+                logging.warning("error while moving the element %s", elt)
+
+    def add_rule(self, element_type, name, value):
+        """Add a sorting rule to the .sortaconfig file.
+        
+        Args :
+            element_type (str, ['prefix'|'ext']) : type of rule to add
+            name (str)                           : name of the rule
+            value (str)                          : destination path of the rule
+
+        """       
+        if element_type == 'prefix':
+            self.config.set('prefix', name, value)
+        else:
+            self.config.set('extension', name, value)
+
+        with open(self.config_path, mode='w') as configfile:
+            self.config.write(configfile)
+
+    def remove_rule(self, element_type, name):
+        """Remove a rule from the .sortaconfig file.
+        
+        Args :
+            element_type (str, ['prefix'|'ext']) : type of rule to add
+            name (str)                           : name of the rule
+        
+        """
+        if element_type == 'prefix':
+            self.config.remove_option('prefix', name)
+        else:
+            self.config.remove_option('extension', name)
+
+        with open(self.config_path, mode='w') as configfile:
+            self.config.write(configfile)
--- sorta/sorta.py
+++ sorta/sorta.py
@@ -1,21 +1,11 @@
 #! /usr/bin/python3
 
-import configparser
 import click
-import logging
-import os
-from shutil import move, Error
-from sys import exit
-from time import time, sleep
-
-CONFIG_FILE_NAME = ".sortaconfig"
+from .drop_folder import DropFolder
 
 @click.group()
 def cli():
-    """Sorta is a tool helping you to manage your files."""
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s [%(levelname)-5.5s]  %(message)s',
-                        handlers=[logging.StreamHandler()])
+    """Sorta is a tool helping you to sort your files."""
 
 @cli.command()
 @click.option('--path', default=".", type=click.Path(exists=True,
@@ -26,22 +16,8 @@ def cli():
                                                      resolve_path=True))
 def sort(path):
     """Sort the files in the drop folder."""
-
-    config_path = os.path.join(path, CONFIG_FILE_NAME)
-    config = get_config(config_path)
-
-    for elt in os.listdir(path):
-        if elt == CONFIG_FILE_NAME:
-            continue
-
-        try:
-            dest = get_destination(elt, config)
-            move(os.path.join(path, elt), dest)
-            logging.info("%s moved to %s", elt, dest)
-        except LookupError as e:
-            logging.warning(str(e))
-        except Error:
-            logging.warning("error while moving the element %s", elt)
+    folder = DropFolder(path)
+    folder.sort()
 
 @cli.command()
 @click.argument('element_type', type=click.Choice(['prefix', 'ext']))
@@ -60,16 +36,8 @@ def sort(path):
                                                      resolve_path=True))
 def add(element_type, name, value, path):
     """Add an entry to the sorting rules."""
-    config_path = os.path.join(path, CONFIG_FILE_NAME)
-    config = get_config(config_path)
-
-    if element_type == 'prefix':
-        config.set('prefix', name, value)
-    else:
-        config.set('extension', name, value)
-
-    with open(config_path, mode='w') as configfile:
-        config.write(configfile)
+    folder = DropFolder(path)
+    folder.add_rule(element_type, name, value)
 
 @cli.command()
 @click.argument('element_type', type=click.Choice(['prefix, ext']))
@@ -82,16 +50,8 @@ def add(element_type, name, value, path):
                                                      resolve_path=True))
 def rm(element_type, name, path):
     """Remove an entry from the sorting rules."""
-    config_path = os.path.join(path, CONFIG_FILE_NAME)
-    config = get_config(config_path)
-
-    if element_type == 'prefix':
-        config.remove('prefix', name)
-    else:
-        config.set('extension', name)
-
-    with open(config_path, mode='w') as configfile:
-        config.write(configfile)
+    folder = DropFolder(path)
+    folder.remove_rule(element_type, name)
 
 @cli.command()
 @click.option('--path', default=".", type=click.Path(exists=False,
@@ -102,41 +62,4 @@ def rm(element_type, name, path):
                                                      resolve_path=True))
 def init(path):
     """Create an empty Sorta drop folder or reinitialize an existing one."""
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-    config = configparser.ConfigParser()
-    config.add_section('core')
-    config.set('core', 'delimiter', '--')
-    config.set('core', 'polling', '60.0')
-
-    config.add_section('prefix')
-
-    config.add_section('extension')
-
-    with open(os.path.join(path, '.sortaconfig'), mode='w') as configfile:
-        config.write(configfile)
-
-def get_config(path):
-    """Parse the config file and return a ConfigParser object."""
-    config = configparser.ConfigParser()
-    config.read_file(open(path))
-
-    return config
-
-def get_destination(element, config):
-    """Find the right destination corresponding to the element name."""
-    split_prefix = element.split(config.get('core', 'delimiter'))
-    split_ext = os.path.splitext(element)
-
-    try:
-        if len(split_prefix) == 2:
-            dest = os.path.join(config.get('prefix', split_prefix[0]), split_prefix[1])
-        elif len(split_ext) == 2:
-            dest = os.path.join(config.get('extension', split_ext[1][1:]), element)
-        else:
-            raise LookupError("no destination found for the element {}".format(element))
-    except configparser.NoOptionError:
-        raise LookupError("no destination found for the element {}".format(element))
-
-    return dest
+    DropFolder.init_folder(path)
